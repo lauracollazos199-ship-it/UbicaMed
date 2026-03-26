eed053126b68b2c729b6e13981bf32d3acebe012
Angie <angiemoreno120106@gmail.com>
2026-03-26 13:15:39 -0500

    Agregado login con Google usando JWT y corrección Pydantic

--- user-service/app/main.py
+++ user-service/app/main.py
@@ -2,13 +2,14 @@ from fastapi import FastAPI
 from app.routes.users import router as user_router
 from app.database.database import Base, engine
 from app.database import models_db
+from app.routes import google_login
 
 app = FastAPI()
 
+app.include_router(google_login.router)
 
 
 Base.metadata.create_all(bind=engine)
-
 
 
 app.include_router(user_router)
