8bc80436b415be8049de2180863ce2eef29c0d32
Laura Daniela Collazos <lauracollazos199@gmail.com>
2026-04-13 17:28:47 -0500

    Requirements correctos para user!

--- user-service/app/database/models_db.py
+++ user-service/app/database/models_db.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String
 from app.database.database import Base
 
 
